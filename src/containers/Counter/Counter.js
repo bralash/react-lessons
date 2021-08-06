435a883ae8d5451890d397ecc2797ebbd8de8b50
Emmanuel Asaber <emmanuelasaber@gmail.com>
2021-08-06 12:36:18 +0000

    Added state to the counter

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -3,14 +3,37 @@ import CounterControl from '../../components/CounterControl/CounterControl'
 import CounterOutput from '../../components/CounterOutput/CounterOutput'
 
 class Counter extends Component {
+    state = {
+        counter: 0
+    }
+
+    counterChangedHandler = ( action, value ) => {
+        switch ( action ) {
+            case 'inc':
+                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
+                break;
+            case 'dec':
+                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
+                break;
+            case 'add':
+                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
+                break;
+            case 'sub':
+                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
+                break;
+            default: 
+                return null;
+        }
+    }
+
     render() {
         return (
             <div>
-                <CounterOutput value="0" />
-                <CounterControl label="Increment" />
-                <CounterControl label="Decrement" />
-                <CounterControl label="Add 5" />
-                <CounterControl label="Subtract 5" />
+                <CounterOutput value={this.state.counter} />
+                <CounterControl label="Increment" clicked={() => this.counterChangedHandler('inc')} />
+                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler('dec')}  />
+                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler('add', 5)}  />
+                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler('sub', 5)}  />
             </div>
         )
     }
